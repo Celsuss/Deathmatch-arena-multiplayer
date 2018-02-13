2499423bbbefb29f6efc096aac3703591252e6db
Jens Lordén <jenslorden@gmail.com>
2018-02-13 22:22:41 +0100

    Cleaned up project.

--- Assets/Scripts/Pickups/SpawnPickup.cs
+++ Assets/Scripts/Pickups/SpawnPickup.cs
@@ -7,19 +7,9 @@ public class SpawnPickup : NetworkBehaviour {
 
 	[SerializeField] PickupBase m_Pickup;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
 	public override void OnStartServer(){
 		GameObject obj = NetworkBehaviour.Instantiate(m_Pickup.gameObject, transform.position, m_Pickup.transform.rotation);
 		NetworkServer.Spawn(obj);
 		obj.SetActive(true);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
